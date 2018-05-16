64cba6871d56c614e7791520734dfdeafc57f9c5
ekin1106 <ekin1106@hotmail.com>
2018-05-16 19:19:57 +0800

    增加wget下载，方便解析地址

--- bcy.py
+++ bcy.py
@@ -1,31 +1,48 @@
 import re
-import urllib.request
-import urllib.request
+# import urllib.request
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 import time
+import wget
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
 # chrome_options = Options()
 # chrome_options.add_argument('--headless')
 # chrome_options.add_argument('--disable-gpu')
 # driver = webdriver.Chrome(chrome_options=chrome_options)
-url = input('please input BCY url:')
+
 # driver = webdriver.Chrome()
 # driver.get(url)
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-web = urllib.request.Request(url,headers=headers)
-page = urllib.request.urlopen(web)
-soup = BeautifulSoup(page,'lxml')
+# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+# web = urllib.request.Request(url,headers=headers)
+# page = urllib.request.urlopen(web)
+def bcy_page(url):
+    web = requests.session()
+    cos_page = web.get(url)
+    cos_page.encoding = 'utf-8'
+    soup = BeautifulSoup(cos_page.text,'lxml')
+    time.sleep(3)
+    soup_find = soup.find_all('img',{'class':'detail_clickable'})#detail_std')
+    download_links = []
+    for get_link in soup_find:
+        img_src = get_link.get('src')
+        re_img_src = re.findall(r'(.*.jpg)',img_src)
+        file_name = re.findall('r.*/(.*.jpg)',img_src)
+        download_links.append(re_img_src[0])
+    return download_links
+    #     urllib.request.urlretrieve(re_img_src[0],file_name[0])
+    #     time.sleep(5)
 
-time.sleep(5)
-# soup = BeautifulSoup(driver.page_source,'lxml')
-# print(driver.page_source)
-soup_find = soup.find_all('img',class_='detail_std')
-for get_link in soup_find:
-    img_src = get_link.get('src')
-    re_img_src = re.findall(r'(.*.jpg)',img_src)
-    file_name = re.findall('r.*/(.*.jpg)',img_src)
-    urllib.request.urlretrieve(re_img_src[0],file_name[0])
-    time.sleep(5)
+def down_pic(link):
+    '''wget模块下载'''
+    wget.download(link)
 
-# driver.close()
+if __name__ == '__main__':
+    url = input('please input BCY url:')
+    link = bcy_page(url)
+    pool = ThreadPool(5)
+    pool.map(down_pic,link)
+    pool.close()
+    pool.join()
